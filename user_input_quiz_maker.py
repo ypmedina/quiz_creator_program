27aa0fa479c9f4aec40f550199648164800dd781
unknown <yaelmedina2031@gmail.com>
2025-04-09 04:43:23 +0800

    Created the program file and put the specifications of the program in a comment within the file

---
+++ user_input_quiz_maker.py
@@ -0,0 +1,7 @@
+"""
+Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d)
+and the correct answer.Write the collected data to a text file.
+Ask another question until the user chose to exit.
+"""
+
+#use the function open() to create the text file
