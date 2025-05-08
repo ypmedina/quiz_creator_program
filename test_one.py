3a19987bf61c27d23b8be24209cd1cce1db94c63
unknown <yaelmedina2031@gmail.com>
2025-05-09 03:10:23 +0800

    Added the function to load in the questions, hopefully it works

--- test_one.py
+++ test_one.py
@@ -3,16 +3,6 @@ from tkinter import Radiobutton, IntVar
 import os
 import json
 
-desktop_path = os.path.join(os.path.expanduser("~"), "Documents", "user_quiz_GUI.json")
-
-try:
-    with open(desktop_path, 'r', encoding='utf=8') as file:
-        quiz_data = json.load(file)
-
-except FileNotFoundError:
-    print("The file is not found. Please make sure to save the quiz")
-    exit()
-
 window = tk.Tk()
 
 bg_color = "#fff9ed"
@@ -25,8 +15,32 @@ window.configure(bg=bg_color)
 header_frame = tk.Frame(window, height=50, bd=1, bg='#fff9ed')
 header_frame.pack(fill='x')
 
-header_label = tk.Label(header_frame, text='Quiz maker', font='Arial, 60', bg='#fff9ed')
+header_label = tk.Label(header_frame, text='Quiz', font='Arial, 60', bg='#fff9ed')
 header_label.pack(anchor="w")
+desktop_path = os.path.join(os.path.expanduser("~"), "Documents", "user_quiz_GUI.json")
 
+try:
+    with open(desktop_path, 'r', encoding='utf=8') as file:
+        quiz_data = json.load(file)
+
+except FileNotFoundError:
+    print("The file is not found. Please make sure to save the quiz")
+    exit()
+
+
+questions = quiz_data["questions"] #from kson
+question_number = 0
+
+
+question_var = tk.StringVar()
+selected = tk.IntVar()
+answer_vars = [tk.StringVar() for _ in range(4)]
+
+def load_question():
+    q = questions[question_number]
+    question_var.set(f"Q{question_number+1}: {q['question']}")
+    for i in range(4):
+        answer_vars[i].set(q["answers"][i])
+    selected.set(0)
 
 window.mainloop()
